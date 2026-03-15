b38d0d7163f3d7a0dfc014dc8000b38807907e1c
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2026-03-15 16:58:57 +0300

    configure server entry and routes

---
+++ backend/src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import passport from "./config/passport.js";
+import authRouter from "./routes/auth-route.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"] }));
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
+app.use(passport.initialize());
+
+app.use("/auth", authRouter);
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  return res.status(500).json({ message: "Something went wrong in server!" });
+});
+
+export default app;
