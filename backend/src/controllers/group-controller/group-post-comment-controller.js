fd1293c9a7e5bf542c31a1be56306c99ee1993a7
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2026-07-01 10:40:02 +0300

    refactor(group):update comment and  new post  check group exist

--- backend/src/controllers/group-controller/group-post-comment-controller.js
+++ backend/src/controllers/group-controller/group-post-comment-controller.js
@@ -1,21 +1,21 @@
 import {
   addComment,
+  updateComment,
   deleteComment,
   getCommentsByPost,
-} from "../../models/group-query/group-post-queries.js";
+  getPostWithTelegramStyleComments,
+} from "../../models/group-query/group-post-reply-query.js";
 import { CommentSchema } from "../../middlewares/validation/schema-validation.js";
 
 // Add comment to post
 async function addCommentToPost(req, res) {
   const { postId } = req.params;
   const result = CommentSchema.safeParse(req.body);
-
   if (!result.success) {
     return res
       .status(400)
       .json({ message: "Invalid comment data", detail: result.error.message });
   }
-
   let userId = req.user.id;
 
   try {
@@ -33,9 +33,16 @@ async function addCommentToPost(req, res) {
 // Delete comment
 async function deleteCommentHandler(req, res) {
   const { commentId } = req.params;
+  const userId = req.user.id;
+
+  if (!commentId || !userId) {
+    return res
+      .status(400)
+      .json({ message: "Comment ID and user ID is required" });
+  }
 
   try {
-    await deleteComment(commentId, req.user.id);
+    const comment = await deleteComment(commentId, userId);
     return res.json({ message: "Comment deleted successfully" });
   } catch (error) {
     console.error(error);
@@ -50,13 +57,45 @@ async function deleteCommentHandler(req, res) {
       .json({ message: `Failed to delete comment: ${error.message}` });
   }
 }
+//update comment
+async function updateCommentHandler(req, res) {
+  const { commentId } = req.params;
+  const result = CommentSchema.partial().safeParse(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({ message: result.error.message });
+  }
+  //pass the author id
+  const { text, userId } = result.data;
+  if (!text) {
+    return res.status(400).json({ message: "Comment text is required" });
+  }
+  try {
+    const comment = await updateComment(commentId, userId, result.data);
+    return res.json(comment);
+  } catch (error) {
+    console.error(error);
+    if (error.message.includes("Not authorized")) {
+      return res.status(403).json({ message: error.message });
+    }
+    if (error.message.includes("not found")) {
+      return res.status(404).json({ message: error.message });
+    }
+    return res
+      .status(500)
+      .json({ message: `Failed to update comment: ${error.message}` });
+  }
+}
 
 // Get comments for a post
 async function getComments(req, res) {
   const { postId } = req.params;
-
+  const { nested } = req.query;
+  if (!postId) {
+    return res.status(400).json({ message: "Post ID is required" });
+  }
   try {
-    const comments = await getCommentsByPost(postId);
+    const comments = await getCommentsByPost(postId, nested);
     return res.json(comments);
   } catch (error) {
     console.error(error);
@@ -66,4 +105,28 @@ async function getComments(req, res) {
   }
 }
 
-export { addCommentToPost, deleteCommentHandler, getComments };
+// Get comments for a post
+async function getCommentsWithTelegramStyle(req, res) {
+  const { postId } = req.params;
+  if (!postId) {
+    return res.status(400).json({ message: "Post ID is required" });
+  }
+
+  try {
+    const comments = await getPostWithTelegramStyleComments(postId);
+    return res.json(comments);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: `Failed to get comments: ${error.message}` });
+  }
+}
+
+export {
+  addCommentToPost,
+  updateCommentHandler,
+  getComments,
+  deleteCommentHandler,
+  getCommentsWithTelegramStyle,
+};
--- backend/src/controllers/group-controller/group-post-controller.js
+++ backend/src/controllers/group-controller/group-post-controller.js
@@ -8,6 +8,7 @@ import {
   deletePost,
 } from "../../models/group-query/group-post-queries.js";
 import {
+  getGroupById,
   isGroupMember,
   isGroupOwner,
 } from "../../models/group-query/group-queries.js";
@@ -29,7 +30,6 @@ async function createNewPostForGroup(req, res) {
       .status(400)
       .json({ message: "Unauthorized to post without userId and groupId" });
   }
-
   const { text, imgUrl, groupId } = result.data;
   if (!text && !imgUrl) {
     return res
@@ -38,6 +38,15 @@ async function createNewPostForGroup(req, res) {
   }
 
   try {
+    const group = await getGroupById(groupId);
+    if (!group) {
+      return res.status(404).json({ message: "Group not found" });
+    }
+    if (group?.isDeleted) {
+      return res
+        .status(404)
+        .json({ message: "Group has been deleted and it is not active." });
+    }
     const isMember = await isGroupMember(groupId, userId);
     const isOwner = await isGroupOwner(groupId, userId);
 
